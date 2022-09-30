03bb0b2971902f9baa3a3bec080d2d59f9dc7281
Andreas Habel <andreas.habel@mail.schwarz>
2022-09-30 09:55:24 +0200

    Added missing metric description.

--- semp/metricDesc.go
+++ semp/metricDesc.go
@@ -90,6 +90,7 @@ var MetricDesc = map[string]Metrics{
 		"system_spool_disk_partition_usage_active_percent": prometheus.NewDesc(namespace+"_"+"system_spool_disk_partition_usage_active_percent", "Total disk usage in percent.", nil, nil),
 		"system_spool_usage_bytes":                         prometheus.NewDesc(namespace+"_"+"system_spool_usage_bytes", "Spool total persisted usage.", nil, nil),
 		"system_spool_usage_msgs":                          prometheus.NewDesc(namespace+"_"+"system_spool_usage_msgs", "Spool total number of persisted messages.", nil, nil),
+		"system_spool_files_utilization_percent":           prometheus.NewDesc(namespace+"_"+"system_spool_files_utilization_percent", "Utilization of spool files in percent.", nil, nil),
 	},
 	"Redundancy": {
 		"system_redundancy_up":           prometheus.NewDesc(namespace+"_"+"system_redundancy_up", "Is redundancy up? (0=Down, 1=Up).", variableLabelsRedundancy, nil),
