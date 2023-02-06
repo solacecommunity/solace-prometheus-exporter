1163d568a682ac051085a8d0609d1ba79f3b9fc5
vivek02172003 <79029055+vivek02172003@users.noreply.github.com>
2023-02-06 15:55:56 -0500

    use package main

--- version.go
+++ version.go
@@ -13,5 +13,5 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package messaging
+package main
 const version = "1.0.0"
